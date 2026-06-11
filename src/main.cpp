494f0b9283364029fde157aa2a8ab1d5e870f01e
Kyle Horn <hornk@purdue.edu>
2026-06-11 16:31:59 -0400

    code for app.rc and resources.qrc added. code also added to main.cpp for handling icon logo usage

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
-#include "mainWindow.h"
-
 #include <QApplication>
+#include <QIcon>
+#include "MainWindow.h"
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
-    return a.exec();
+    QApplication app(argc, argv);
+    app.setApplicationName("Infinity Creator");
+    app.setWindowIcon(QIcon(":/infinity_creator.ico"));   // title bar, taskbar, Alt+Tab
+
+    MainWindow window;
+    window.setWindowTitle("Infinity Creator");
+    window.show();
+    return app.exec();
 }
