a6b2cd5527cb96934bf76c6d9df9e3b900e56c91
Kyle Horn <hornk@purdue.edu>
2026-06-15 17:57:22 -0400

    Fix formatting and clean up comments in OcctViewport header file

--- src/OcctViewport.h
+++ src/OcctViewport.h
@@ -2,27 +2,27 @@
 
 #include <QWidget>
 
-//The following included files use the .hxx file extensions to match with OpenCASCADE
+// The following included files use the .hxx file extensions to match with OpenCASCADE
 #include <V3d_Viewer.hxx>
 #include <V3d_View.hxx>
 #include <AIS_InteractiveContext.hxx>
 
-
-
 class OcctViewport : public QWidget
 {
     Q_OBJECT
-    
 
 public:
-    explicit OcctViewport(QWidget* parent = nullptr);
+    explicit OcctViewport(QWidget *parent = nullptr); // asdfaposdifha
+
+protected:
+    QPaintEngine *paintEngine() const override;   // "Qt, dont paint here"
+    void paintEvent(QPaintEvent *event) override; // Draw the occt view
 
 private:
-    Handle(V3d_Viewer) ;
-    Handle(V3d_View) ;
+    Handle(V3d_Viewer);
+    Handle(V3d_View);
     Handle(AIS_InteractiveContext);
     bool m_initialized = false;
 
 protected:
-
 };
