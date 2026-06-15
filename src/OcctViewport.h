6b2e95d672a1a98614db45bcbcc7d670acde8238
Kyle Horn <hornk@purdue.edu>
2026-06-15 17:33:04 -0400

    Refactor OcctViewport class to improve structure and include necessary headers

--- src/OcctViewport.h
+++ src/OcctViewport.h
@@ -1,12 +1,29 @@
 #pragma once
 
 #include <QWidget>
+#include <QMainWindow>
+
+//The following included files use the .hxx file extensions to match with OpenCASCADE
+#include <V3d_Viewer.hxx>
+#include <V3d_View.hxx>
+#include <AIS_InteractiveContext.hxx>
+
 
 
 class OcctViewport : public QWidget
 {
     Q_OBJECT
+    
 
 public:
     explicit OcctViewport(QWidget* parent = nullptr);
+
+private:
+    Handle(V3d_Viewer) m_viewport;
+    Handle(V3d_View) m_;
+    Handle(AIS_InteractiveContext);
+    bool m_initialized = false;
+
+protected:
+
 };
